1b17997bff2a8593a06691ab1ca03b35473b2f7d
Philip A Cook <cookpa@pennmedicine.upenn.edu>
2024-09-13 16:35:05 -0400

    BUG: tree

--- antsnetct/bids_helpers.py
+++ antsnetct/bids_helpers.py
@@ -892,7 +892,7 @@ def find_participant_images(input_dataset_dir, participant_label, work_dir, vali
     # Make a temp directory and set up a virtual BIDS dir
     tmp_dataset_dir = _make_virtual_participant_dataset(input_dataset_dir, participant_label, work_dir)
 
-    bids_tree = run_command(['tree', '{tmp_dataset_dir}'])
+    bids_tree = run_command(['tree', 'tmp_dataset_dir'])
 
     print(bids_tree['stdout'])
 
