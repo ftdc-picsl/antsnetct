af624cfe4e3303749a4d4414b8d1c67c152c23f2
Philip Cook <cookpa@pennmedicine.upenn.edu>
2024-09-27 15:06:59 -0400

    Multimodal deep atropos (#32)
    * ENH big improvements to deep_atropos T1w processing
    
    Also fix multi-threading, rely on environment vars - safest way to ensure threads are set before import

--- antsnetct/__init__.py
+++ antsnetct/__init__.py
@@ -1,11 +1,5 @@
 # __init__.py
 
-import tensorflow as tf
-
 from .log_config import configure_logging
-from .system_helpers import set_num_threads
 
 configure_logging()
-
-# Set default number of threads - this might be overidden by the user
-set_num_threads()
