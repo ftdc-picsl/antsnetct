d7490bb298935d782de2e3bbd214ac566425ec9a
Philip A Cook <cookpa@pennmedicine.upenn.edu>
2025-09-22 08:50:59 -0400

    ENH: parcellation and label stats
    Tested:
    
      - antspynet DKT31, HOA, cerebellum
      - template labels with and without cortical propagation
    
    Need to test:
    
      - secondary templates (eg, labels from MNI through another template)
      - longitudinal

--- antsnetct/__init__.py
+++ antsnetct/__init__.py
@@ -4,6 +4,9 @@ from .log_config import configure_logging
 
 configure_logging()
 
+from . import cross_sectional_pipeline
+from . import longitudinal_pipeline
+from . import parcellation_pipeline
 from . import ants_helpers
 from . import bids_helpers
 from . import preprocessing
